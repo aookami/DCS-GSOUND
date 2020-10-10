57c29d7081a7968f11f0c7d51f9bd0f1e8fde1c0
aookami <mababa3003@gmail.com>
2020-10-10 00:22:32 -0300

    Update Export.lua

--- Export.lua
+++ Export.lua
@@ -1,8 +1,3 @@
-local lfs=require('lfs');
-dofile(lfs.writedir()..[[Scripts\pw-dev_script\Export.lua]])
-pcall(function() local dcsSr=require('lfs');dofile(dcsSr.writedir()..[[Mods\Services\DCS-SRS\Scripts\DCS-SimpleRadioStandalone.lua]]); end,nil);
-
-
 
 function LuaExportStart()
 	socket = require("socket")
@@ -19,5 +14,3 @@ end
 function LuaExportStop()
 	connection:close()
 end
-
-BIOS = {}; BIOS.LuaScriptDir = [[P:\dcsb\dcs-lua\]]; BIOS.PluginDir = [[C:\Users\mabaw\AppData\Roaming/DCS-BIOS/Plugins\]]; if lfs.attributes(BIOS.LuaScriptDir..[[BIOS.lua]]) ~= nil then dofile(BIOS.LuaScriptDir..[[BIOS.lua]]) end --[[DCS-BIOS Automatic Setup]]
